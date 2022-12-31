a39d3209a01d2b3be8510ead7c99d5de6f502318
KarimMuhamad <karimmuhammad551@gmail.com>
2022-12-31 13:05:24 +0700

    Add some features in to do

--- client.py
+++ client.py
@@ -39,38 +39,47 @@ def clear():
         _ = system('clear')
 
 def countDown(endtime) :
-    if(datetime.now() >= endtime) :
+    if(datetime.now() >= endtime):
         return "------"
     elif(endtime > datetime.now()) :
         return endtime - datetime.now()
+    else :
+        return "Done"
     
 
-def checkStatus(endtime) :
-    if(datetime.now() >= endtime) :
-        return "Berakhir"
-    elif(endtime > datetime.now()) :
-        return "Belum Selesai"
+def checkStatus(lsTd) :
+    if(datetime.now() >= lsTd["Waktu Berakhir"] and lsTd["Status"] != "Selesai") :
+        lsTd["Status"] = "Berakhir"
+    elif(lsTd["Waktu Berakhir"] > datetime.now() and lsTd["Status"] != "Selesai") :
+        lsTd["Status"] = "Belum Selesai"
 
-while 1 :
-    print("To Do List App")
+
+def printTodo(listOfTodo) :
     idx =1
     for i in lsTodo :
-        i["Status"] = checkStatus(i["Waktu Berakhir"])
+        checkStatus(i)
 
         print(f'[{idx}]. {i["Nama"]} \t {i["Status"]} \t {countDown(i["Waktu Berakhir"])}')
         idx+=1
 
+while 1 :
+    print("To Do List App")
+    printTodo(lsTodo)
+
+
     print("""
     1 : Add To Do
-    2 : Remove To Do
-    3 : Exit
+    2 : Check Todo
+    3 : Remove To Do
+    4 : Log
+    5 : Exit
     """)
     inp = int(input("Masukan Pilihan : "))
 
     clear()
     if(inp == 1) :
         nama = input("Masukan nama todo : ")
-        d, m, y = map(int, input("Masukan tanggal mulai (23/12/2022) : ").split("/"))
+        d, m, y = datetime.now().day, datetime.now().month, datetime.now().year
         hr, mnt = map(int, input("Masukan Jam Mulai (HH:MM): ").split(":"))
         tdoEndDate = datetime(y, m, d, hr, mnt)
 
@@ -83,9 +92,39 @@ while 1 :
 
         lsTodo.append(todo)
         send(["Add", todo])
+        clear()
+
     elif(inp == 2) :
+        printTodo(lsTodo)
+
+        src = input("Checked To Do :")
+
+        for i in lsTodo :
+            if i["Nama"] == src :
+                i["Status"] = "Selesai"
+                print(i)
+                send(["Checked", i])
+                # clear()
+            else :
+                print("Todo List tidak di temukan")
+                clear()
+
+    elif(inp == 3) :
+        printTodo(lsTodo)
+
         src = input("Hapus :")
-        lsTodo.remove(src)
-        send(["Delete", src])
-    elif inp == 3 : 
-        exit()
+
+        for i in lsTodo :
+            if i["Nama"] == src :
+                lsTodo.remove(i)
+                send(["Delete", i])
+                clear()
+            else :
+                print("Todo List tidak di temukan")
+                clear()
+
+        
+    elif inp == 4 : 
+        print("Log disini")
+        input("")
+        clear()
